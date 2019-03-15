bdc92039d3f07d7b5b6405e4a4b9b62bffafd801
zaykasamatova1998 <33778178+zaykasamatova1998@users.noreply.github.com>
2019-03-16 00:25:43 +0500

    Update sucker.php

--- sucker.php
+++ sucker.php
@@ -34,29 +34,44 @@
 </head>
 <body>
 	
-	<?php if(!$error): ?>
-	<h1>Thanks sucker</h1>
+	<?php
+if(!isset($_POST['name'])||!isset ($_POST['section'])||!isset($_POST['cardnumber'])||!isset($_POST['cardtype'])) {
+	?>
+	<h1>Sorry</h1>
+	<p>
+	You didn't fill out the form completely.<a href="buyagrade.html">Try again?</a>
+	</p>
+	
+<?php 
+}else{
+	?>
+	<h1>Thanks, sucker</h1>
 	<p>Your information has been recorded</p>
 
 	<dl>
 		<dt>Name</dt>
 		<dd>
-			<?= $name ?>
+			<?= $_POST['name']?>
 		</dd>
 
 		<dt>Section</dt>
 		<dd>
-			<?= $section ?>
+			<?= $_POST['section']?>
 		</dd>
 
 		<dt>Credit Card</dt>
 		<dd>
-			<?= $card." ($cardType)" ?>
+			<?= $_POST['cardnumber'] ?> (<?=$_POST['cardtype']?>)
 		</dd>
 
 	</dl>
 
 	<p>Here are all the suckers who have submitted here:</p>
+
+<?php 
+$data = $_POST['name'].";".$_POST['section'].";".$_POST['cardnumber'].";".$_POST['cardtype']."\n";
+file_put_contents("sucker.txt", $data, FILE_APPEND);
+?>
 
 	<pre><?= file_get_contents("suckers.txt") ?>
 	</pre>
@@ -66,7 +81,7 @@
 	<h1>Sorry</h1>
 	<p>You did not fill the form correctly. <a href="/buyagrade.html">Try again?</a></p>
 
-	<?php endif; 
+	<?php end if; 
 	?>
 
 </body>
